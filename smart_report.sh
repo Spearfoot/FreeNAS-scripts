34367abe83c35a06201b09df8e452e6efb7029dd
Keith Nash <Spearfoot@users.noreply.github.com>
2017-03-21 13:25:19 -0500

    Fix sendmail bug for compatibility with Corral

--- smart_report.sh
+++ smart_report.sh
@@ -140,6 +140,7 @@ echo "</pre>" >> ${logfile}
 if [ -z "${email}" ]; then
   echo "No email address specified, information available in ${logfile}"
 else
-  sendmail -t < ${logfile}
+#  sendmail -t < ${logfile}
+  sendmail ${email} < ${logfile}
   rm ${logfile}
 fi
